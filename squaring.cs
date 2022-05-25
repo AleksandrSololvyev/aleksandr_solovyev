92476ba640346bd9a8217ec4ba5c76c08d3e092e
Aleksandr_Solovyev <105586578+AleksandrSololvyev@users.noreply.github.com>
2022-05-26 00:42:52 +0300

    Add files via upload

---
+++ squaring.cs
@@ -0,0 +1,18 @@
+﻿ //Напишите цикл, который принимает на вход два числа 
+ //(A и B) и возводит число A в натуральную степень B.
+ // 3, 5 -> 243 (3⁵)
+ //2, 4 -> 16
+ int result, i;
+Console.WriteLine("Введите число, которое хотите возвести в степень");
+ int number = int.Parse(Console.ReadLine());
+ Console.WriteLine("Введите степень этого числа");
+ int degree = int.Parse(Console.ReadLine());
+ result = number;
+ for (i = 1; i < degree; i++)
+{
+ result = result * number;
+}
+{
+ Console.WriteLine($"Полученное число: {result}");
+}
+
