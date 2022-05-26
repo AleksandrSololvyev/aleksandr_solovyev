6f8f350e9cb521958be05e687ce257691d092f0a
Aleksandr_Solovyev <105586578+AleksandrSololvyev@users.noreply.github.com>
2022-05-26 17:08:10 +0300

    Add files via upload

---
+++ 10.Even_numbers.cs
@@ -0,0 +1,40 @@
+﻿//Задайте массив заполненный случайными положительными трёхзначными 
+//числами. Напишите программу, которая 
+//покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+int i;
+int [] InitArray(int length)
+{
+    int [] arr = new int[length];
+    Random rnd = new Random();
+    for(i = 0; i < length; i++)
+    {
+        arr[i] = rnd.Next(100, 999);
+    }
+
+    return arr;
+}
+   int GetCountOfEvenNumbers(int [] arr)
+   {
+       int summ = 0;
+       foreach(int item in arr)
+       {
+           if(item % 2 == 0)
+           summ += 1;
+       }
+       return summ;
+   }
+
+   void PrintArray(int [] arr)
+   {
+       foreach(int item in arr)
+       Console.Write($"{item} ");
+   }
+Console.WriteLine("Введите размерность массива:");
+int length = int.Parse(Console.ReadLine());
+int[] arr = InitArray(length);
+Console.WriteLine("Полученный массив:");
+PrintArray(arr);
+int summ = GetCountOfEvenNumbers(arr);
+Console.WriteLine();
+Console.WriteLine($"Количество чётных чисел в массиве: {summ}");
